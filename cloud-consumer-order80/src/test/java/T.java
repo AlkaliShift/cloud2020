96256f63e5f1b992fb174fe53fb1721fc70d6fce
Alkali_Shift <swordsgod1111@icloud.com>
2020-08-25 13:35:15 +0800

    cloud2020 完成ribbon自定义轮询负载均衡

---
+++ cloud-consumer-order80/src/test/java/T.java
@@ -0,0 +1,10 @@
+/**
+ * @author shenghui
+ * @version 1.0
+ * @since 2020/8/24 17:10
+ */
+public class T {
+    public static void main(String[] args){
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
