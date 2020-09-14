c8fdcc21490c74a3d98ba4bcaa697962ab9688ef
Alkali_Shift <swordsgod1111@icloud.com>
2020-09-14 14:36:20 +0800

    cloud2020 完成Gateway网关断言过滤

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,39 @@
+package com.atguigu.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @author shenghui
+ * @version 1.0
+ * @since 2020/9/9 16:54
+ */
+@Component
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("*****come in MyLogGatewayFilter: " + new Date());
+        String uName = exchange.getRequest().getQueryParams().getFirst("uName");
+        if (uName == null){
+            log.info("*****用户名为null，非法用户，o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
