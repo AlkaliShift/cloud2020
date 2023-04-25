1cea8b2b7fd10661f23faa7c21cea781a1bb2116
shwu6127 <shwu6127@uni.sydney.edu.au>
2023-04-25 16:51:28 +0800

    cloud2020 完成Nacos服务注册与调用

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shenghui
+ * @version 1.0
+ * @since 2023/4/25 14:32
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}
